8b7dad119e9fbc1c7c7d7dff93ee91f274b1c345
Jim Barnes <jimbarnesdeveloper@gmail.com>
2024-05-02 12:04:32 -0400

    Bumped version for testing

--- github-action-testing.php
+++ github-action-testing.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Github Action Testing
 Description: This plugin provides no functionality and is only used for testing deploying using github actions.
-Version: 1.0.0
+Version: 1.0.2
 Author: UCF Web Communications
 License: GPL3
 GitHub Plugin URI: /UCF/github-action-testing
